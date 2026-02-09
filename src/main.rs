0fe03f059d38bad24b8058efc8bb12dc76136d58
mnmaita <47983254+mnmaita@users.noreply.github.com>
2026-02-09 21:54:46 +0100

    Adds bevy dependency and compilation config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Bevy Jam 7".to_string(),
+            ..Default::default()
+        }),
+        ..Default::default()
+    }));
+
+    app.run();
 }
